2ea775889ef55556fcc4ce68c875efb67c4e9db8
Jan-David Schindler <jandavidschindler@gmail.com>
2018-06-15 17:50:33 +0200

    create spring project

---
+++ src/main/java/spotifire/SpotifireApplication.java
@@ -0,0 +1,12 @@
+package spotifire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifireApplication.class, args);
+    }
+}
